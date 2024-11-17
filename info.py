79567c8cbb137d594183c5458c179bb0d34eee7a
DASARIPARDHASARADHI <95266626+DASARIPARDHASARADHI@users.noreply.github.com>
2024-11-17 23:07:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -157,7 +157,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://web-wkvaph8oezoq.up-de-fra1-k8s-1.apps.run-on-seenode.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
